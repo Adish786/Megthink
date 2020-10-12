aac14354ccb1706378f6240f648296d7a8c6a5da
Adish Ansari <adishansari786@gmail.com>
2020-10-12 21:37:30 +0530

    Add files via upload

---
+++ MegthinkAssignmentOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.MegthinkAssignmentOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegthinkAssignmentOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
